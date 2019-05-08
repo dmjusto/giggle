6e9bb2fc191e63fa10b4754ba91a22d98d19bad0
Dylan <dylanfairbanks@icloud.com>
2019-05-08 15:19:10 -0600

    added converting relative links to absolute links

--- googleclone/crawl.php
+++ googleclone/crawl.php
@@ -2,9 +2,29 @@
 
 include("classes/DomDocumentParser.php");
 
-//this is a test
 function createLink($src,$url){
 
+  $scheme = parse_url($url)["scheme"];
+  $host = parse_url($url)["host"];
+
+  if (substr($src,0,2)=="//"){
+    $src = $scheme . ":" . $src;
+  }
+  elseif (substr($src,0,1)=="/"){
+    $src  = $scheme . "://" . $host . $src;
+  }
+  elseif (substr($src,0,2) == "./"){
+    $src = $scheme . "://" . $host . dirname(parse_url($url)["path"]) . substr($src,1);
+  }
+  elseif (substr($src,0,3) == "../"){
+    $src = $scheme . "://" . $host . "/" . $src;
+  }
+  elseif (substr($src,0,5) != "https" && substr($src,0,4) != "http"){
+    $src = $scheme . "://" . $host . "/" . $src;
+  }
+
+
+  return $src;
 }
 
 function followLinks($url){
@@ -21,8 +41,8 @@ function followLinks($url){
       continue;
     }
 
-    createLink($href,$url);
-//    echo $href . "<br>";
+    $href = createLink($href,$url);
+    echo $href . "<br>";
   }
 }
 
