47c22d2f4ad163d49b0f88114e49f0a493425d2a
Dylan Fairbanks <31903571+dmjusto@users.noreply.github.com>
2019-05-07 20:55:24 -0600

    initial commit adding files via upload

---
+++ googleclone/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Giggle Search</title>
+
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+</head>
+<body>
+
+	<div class="wrapper indexPage">
+
+		<article class="mainSection">
+
+			<section class="logoContainer">
+				<img src="assets/img/giggleLogo.png" >
+			</section>
+
+			<section class="searchContainer">
+				<form class="" action="search.php" method="GET">
+
+					<input type="text" name="term" class="searchBox">
+					<input type="submit" value="search" class="searchButton">
+
+				</form>
+
+			</section>
+
+		</article>
+
+	</div>
+
+</body>
+</html>
