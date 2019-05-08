c749d23b968e9a81087be1f4990195d87657cd4f
Dylan <dylanfairbanks@icloud.com>
2019-05-08 12:08:47 -0600

    moved local repository

--- googleclone/classes/DomDocumentParser.php
+++ googleclone/classes/DomDocumentParser.php
@@ -4,9 +4,20 @@
 class DomDocumentParser
 {
 
+  private $doc;
   public function __construct($url)
   {
-    echo "URL: $url";
+    $options = array('http' => array('method' => "GET", 'header' => "User-Agent: giggleBot/0.1\n"));
+
+    $context = stream_context_create($options);
+
+    $this->doc = new DomDocument();
+    @$this->doc->loadHTML(file_get_contents($url, false, $context));
+  }
+
+
+  public function getLinks(){
+    return $this->doc->getElementsByTagName("a");
   }
 }
 
--- googleclone/crawl.php
+++ googleclone/crawl.php
@@ -4,6 +4,12 @@ include("classes/DomDocumentParser.php");
 
 function followLinks($url){
   $parser = new DomDocumentParser($url);
+
+  $linksList = $parser->getLinks();
+  foreach ($linksList as $link) {
+    $href = $link->getAttribute("href");
+    echo $href . "<br>";
+  }
 }
 
 $startUrl = "http://www.reecekenney.com";
